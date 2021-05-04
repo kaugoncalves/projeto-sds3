a3e3b9c5b0b0beab32649e05c58b50329954fc52
Kauan Goncalves <kauangoncalves12.ks@gmail.com>
2021-05-03 22:53:27 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div><h1> Ola mundo!! kkkkk </h1></div>
+    <div className="text-primary"><h1> Ola mundo!! kkkkk </h1></div>
   );
 }
 
