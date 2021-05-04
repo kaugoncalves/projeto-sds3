503b87a636d0d9a6b13c20896ba40034cac35ac0
Kauan Goncalves <kauangoncalves12.ks@gmail.com>
2021-05-03 23:44:36 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
 
 function App() {
   return (
-    <div className="text-primary"><h1> Ola mundo!! kkkkk </h1></div>
+    <>
+      <div className="container">
+        <NavBar />
+        <h1> Ola mundo!! kkkkk </h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://github.com/kaugoncalves"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kauan Gonçalves
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/kauans_"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @kauans_
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
