63b31089d51517c941ec157aa0d4fe5a6fef5e5a
Kauan Goncalves <kauangoncalves12.ks@gmail.com>
2021-05-16 19:54:25 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+  id: number,
+  visited: number,
+  deals: number,
+  amount: number,
+  date: string,
+  seller: Seller
+}
+
+export type SalePage = {
+  content?: Sale[],
+  last: boolean,
+  totalPages: number,
+  totalElements: number,
+  size?: number,
+  number: number,
+  first: boolean,
+  numberOfElements?: number,
+  empty?: boolean
+}
+
 export type SaleSum =  {
   sellerName: string,
   sum: number
@@ -8,3 +31,4 @@ export type SaleSucess = {
   visited: number,
   deals: number
 }
+
